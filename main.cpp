e1a991e8060d13c3b4551da6c97a60bc3f76e32f
umut <umut@gamegos.com>
2018-12-07 17:26:05 +0300

    ilk() metodunu yaz

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 
 class kural {
@@ -35,9 +36,13 @@ class gramer {
         std::vector<kural> kayit_tablosu;
     public:
         explicit gramer(std::string kural_satirlari);
-        void yazdir();
-        std::set<std::string> ilk(std::string &katar);
+        void yazdir() const;
+        std::string acilim_bul(const std::string &nt) const;
+        std::set<std::string> ilk(std::vector<std::string> &katar);
+        void ilk(std::vector<std::string> &katar, std::set<std::string> &ilk_kumesi, std::vector<std::string> bakildi);
         std::set<std::string> izle(std::string &nt);
+        bool non_terminal_mi(std::string &s);
+        std::vector<std::vector<std::string>> acilimlar(std::string &nt, std::vector<std::string> &bakildi);
 };
 
 gramer::gramer(std::string kural_satirlari) {
@@ -75,7 +80,7 @@ gramer::gramer(std::string kural_satirlari) {
     }
 }
 
-void gramer::yazdir() {
+void gramer::yazdir() const {
     for (const auto &kayit : kayit_tablosu) {
         std::cout << kayit.non_terminal()  << " : ";
         for (const auto &simge : kayit.acilim()) {
@@ -85,9 +90,76 @@ void gramer::yazdir() {
     }
 }
 
-std::set<std::string> gramer::ilk(std::string &katar) {
-    std::set<std::string> ilk_kumesi;
-    return ilk_kumesi;
+std::string gramer::acilim_bul(const std::string &nt) const {
+    for (const auto &kayit : kayit_tablosu) {
+        if (kayit.non_terminal() == nt) {
+            if (islower(kayit.acilim()[0][0])) {
+                acilim_bul(kayit.acilim()[0]);
+                continue;
+            }
+        }
+    }
+    std::string acilim;
+    return acilim;
+}
+
+std::set<std::string> gramer::ilk(std::vector<std::string> &katar) {
+    std::set<std::string> i;
+    std::vector<std::string> bakildi;
+    ilk(katar, i, bakildi);
+    return i;
+}
+
+bool gramer::non_terminal_mi(std::string &s) {
+    for ( auto const &kayit : kayit_tablosu) {
+        if (kayit.non_terminal() == s) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<std::vector<std::string>> gramer::acilimlar(std::string &nt, std::vector<std::string> &bakildi) {
+    std::vector<std::vector<std::string>> ac;
+    for (auto const &kayit : kayit_tablosu) {
+        if (kayit.non_terminal() == nt) {
+            if (std::find(bakildi.begin(), bakildi.end(), kayit.non_terminal()) == bakildi.end()) {
+                ac.push_back(kayit.acilim());
+            }
+        }
+    }
+    return ac;
+}
+
+void gramer::ilk(std::vector<std::string> &katar, std::set<std::string> &ilk_kumesi, std::vector<std::string> bakildi) {
+    /*
+    if (islower(katar[0][0])) {
+        ilk_kumesi.insert(katar[0]);
+        return;
+    }
+    for (const auto &kayit : kayit_tablosu) {
+        if (kayit.non_terminal() == katar[0]) {
+            if (islower(kayit.acilim()[0][0])) {
+                ilk_kumesi.insert(kayit.acilim()[0]);
+            }
+        }
+    }
+    for (const auto &kayit : kayit_tablosu) {
+        if (kayit.non_terminal() == katar[0]) {
+            if (islower(kayit.acilim()[0][0])) {
+                ilk_kumesi.insert(kayit.acilim()[0]);
+            }
+        }
+    }*/
+    if (!non_terminal_mi(katar[0])) {
+        ilk_kumesi.insert(katar[0]);
+        return;
+    }
+    std::vector<std::vector<std::string>> ac = acilimlar(katar[0], bakildi);
+    for (auto &a : ac) {
+        bakildi.push_back(katar[0]);
+        ilk(a, ilk_kumesi, bakildi);
+    }
 }
 
 std::set<std::string> gramer::izle(std::string &nt) {
@@ -107,5 +179,17 @@ int main() {
     giris.close();
     gramer LR1(ss.str());
     LR1.yazdir();
+    std::cout << std::endl;
+    std::vector<std::string> v;
+   // v.emplace_back("a");
+    v.emplace_back("S’");
+    v.emplace_back("b");
+    v.emplace_back("a");
+    v.emplace_back("A");
+    std::set<std::string> ik = LR1.ilk(v);
+    for (auto const &e : ik) {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
